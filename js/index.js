66fc6a08f603e0c8bc4b02d8e5180cb367dcdc91
kyphong2k <80156896+kyphong2k@users.noreply.github.com>
2022-04-22 17:34:36 +0700

    update: finish navbar mobile and responsive homepage.

--- js/index.js
+++ js/index.js
@@ -1,22 +1,43 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
+  const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector(".navbar").classList.add("smaller");
-    document.querySelector(".scroll-top-btn").classList.add("open")
+    if(width > 767) {
+      $(".navbar").classList.add("smaller");
+    }
+    $(".scroll-top-btn").classList.add("open")
   } else {
-    document.querySelector(".navbar").classList.remove("smaller");
-    document.querySelector(".scroll-top-btn").classList.remove("open")
+    $(".navbar").classList.remove("smaller");
+    $(".scroll-top-btn").classList.remove("open")
 
     
   }
 } 
 
-const openSearchForm = document.querySelector(".navbar__right-search").addEventListener('click', function() {
-    const headerSearch = document.querySelector(".header-search").classList.toggle("toggled");
+const openSearchForm = $(".navbar__right-search").addEventListener('click', function() {
+    const headerSearch = $(".header-search").classList.toggle("toggled");
 
 })
+// scroll when click button scroll op top
 function scrollOnTop () {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0;
 }
+
+function showMenuMobile(element) {
+  element.classList.toggle('active');
+  const navbarHeight = $(".navbar").style.height = 'auto';
+  const navbarMobileList = $(".navbar__mobile-list").classList.toggle("open");
+
+}
+function showSubmenu(element) {
+  element.parentElement.classList.toggle("open");
+}
+function showSupermenu(element) {
+  element.parentElement.classList.toggle("open");
+}
+// underline navbar active
+
